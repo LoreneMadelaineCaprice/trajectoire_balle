0de723cb6319f36787360f622eb9df8417d31a42
lomac20 <152649910+LoreneMadelaineCaprice@users.noreply.github.com>
2024-08-06 14:34:37 +0000

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 
+import matplotlib.pyplot as plt
+
+def trajectoire_balle(x):
+    position = 10*x - 5*(x**2) 
+    return position
+
+xs = [x/100 for x in list(range(201))]
+ys = [trajectoire_balle(x) for x in xs]
+plt.plot(xs, ys)
+plt.title('Trajectoire d\'une balle lancée')
+plt.xlabel('Position horizontale de la balle')
+plt.ylabel('Position verticale de la balle')
+plt.axhline(y = 0) #dessine une ligne horizontale à travers l'axe y
+plt.show()
+plt.savefig('trajectoire_balle.png')
